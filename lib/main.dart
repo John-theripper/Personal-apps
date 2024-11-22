16bceb24043ffd8e2d3aa16fac29f67ca5571bb3
John Theripper <hkzero2542@gmail.com>
2024-11-22 19:56:01 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'page/login_screen.dart';
+import 'page/home.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'page/notification_service.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  NotificationService notificationService = NotificationService();
+  await notificationService.init();
+  await GetStorage.init();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+ColorScheme defaultColorScheme = const ColorScheme(
+  primary: Color(0xffBB86FC),
+  secondary: Color(0xff03DAC6),
+  surface: Color(0xff181818),
+  error: Color(0xffCF6679),
+  onPrimary: Color(0xff000000),
+  onSecondary: Color(0xff000000),
+  onSurface: Color(0xffffffff),
+  onError: Color(0xff000000),
+  brightness: Brightness.dark,
+);
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final box = GetStorage();
+    return MaterialApp(
+        title: 'Emergency Accident Alert',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: defaultColorScheme,
+          primarySwatch: Colors.blue,
+        ),
+        home: ((box.read('email') != '' || box.read('email') != null)
+            ? const LoginPage(title: 'Login UI')
+            : const HomePage(title: 'HomePage UI')));
+  }
+}
---
+++ lib/page/notification_service.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:alert_app/main.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+
+import '../details_page.dart';
+
+class NotificationService {
+  // Singleton pattern
+  static final NotificationService _notificationService =
+      NotificationService._internal();
+  factory NotificationService() {
+    return _notificationService;
+  }
+
+  NotificationService._internal();
+
+  static const String channelId = "1234900";
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  static const AndroidNotificationDetails _androidNotificationDetails =
+      AndroidNotificationDetails(
+    channelId,
+    "test-001",
+    channelDescription: "This channel is responsible for all the local notifications",
+    playSound: true,
+    priority: Priority.high,
+    importance: Importance.high,
+  );
+
+  static const DarwinNotificationDetails _iOSNotificationDetails =
+      DarwinNotificationDetails();
+
+  final NotificationDetails notificationDetails = const NotificationDetails(
+    android: _androidNotificationDetails,
+    iOS: _iOSNotificationDetails,
+  );
+
+  Future<void> init() async {
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    const DarwinInitializationSettings iOSInitializationSettings =
+        DarwinInitializationSettings(
+      defaultPresentAlert: true,
+      defaultPresentBadge: true,
+      defaultPresentSound: true,
+    );
+
+    const InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: initializationSettingsAndroid,
+      iOS: iOSInitializationSettings,
+    );
+
+    // Initialize timezone here
+    tz.initializeTimeZones();
+
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse: onSelectNotification,
+    );
+  }
+
+  Future<void> requestIOSPermissions() async {
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            IOSFlutterLocalNotificationsPlugin>()
+        ?.requestPermissions(
+          alert: true,
+          badge: true,
+          sound: true,
+        );
+  }
+
+  Future<void> showNotification(
+      int id, String title, String body, String payload) async {
+    await flutterLocalNotificationsPlugin.show(
+      id,
+      title,
+      body,
+      notificationDetails,
+      payload: payload,
+    );
+  }
+
+  Future<void> scheduleNotification(int id, String title, String body,
+      DateTime eventDate, TimeOfDay eventTime, String payload,
+      [DateTimeComponents? dateTimeComponents]) async {
+    final scheduledTime = eventDate.add(Duration(
+      hours: eventTime.hour,
+      minutes: eventTime.minute,
+    ));
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      id,
+      title,
+      body,
+      tz.TZDateTime.from(scheduledTime, tz.local),
+      notificationDetails,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+      androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
+      payload: payload,
+      matchDateTimeComponents: dateTimeComponents,
+    );
+  }
+
+  Future<void> cancelNotification(int id) async {
+    await flutterLocalNotificationsPlugin.cancel(id);
+  }
+
+  Future<void> cancelAllNotifications() async {
+    await flutterLocalNotificationsPlugin.cancelAll();
+  }
+}
+
+Future<void> onSelectNotification(NotificationResponse notificationResponse) async {
+  String? payload = notificationResponse.payload;
+  await navigatorKey.currentState
+      ?.push(MaterialPageRoute(builder: (_) => DetailsPage(payload: payload)));
+}
